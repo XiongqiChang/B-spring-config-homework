8f5496c6c816283619fbb01f945d60a337b9c935
XiongqiChang <2838159948@qq.com>
2020-10-27 17:19:10 +0800

    完成作业

---
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest.java
@@ -0,0 +1,28 @@
+package com.thoughtworks.capability.gtb.demospringconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(properties = "levelNumber=0")
+class LevelControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getLevel() throws Exception {
+        mockMvc.perform(get("/level"))
+                .andExpect(content().string(containsString("basic")));
+    }
+}
